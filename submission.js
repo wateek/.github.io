77d8bdc45ef9dad48b7770a5abd2015b9a933fd5
wateek <wr07jone@siena.edu>
2015-09-30 11:24:31 -0400

    Create submission.js

---
+++ submission.js
@@ -0,0 +1,18 @@
+function clickHandler(){
+	var field = document.getElementById("name");
+	if (field.value == "") {
+		var newNode = document.createElement("span");
+		newNode.innerHTML = "Please enter name.";
+		newNode.style.color = "red";
+		newNode.id = "name_error";
+		field.parentNode.insertBefore(newNode, field.nextSibling);
+		var label = document.querySelectorAll('[for="name"]')[0];
+		label.style.color = "red";
+	}
+	else {
+		var label = document.querySelectorAll('[for="name"]')[0];
+		label.style.color = "black";
+		var message = document.getElementById("name_error");
+		message.parentNode.removeChild(message);		
+	}
+}
